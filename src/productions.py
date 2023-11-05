464a16703935ff226b5ba5d6473df60b42dda572
daniel <dvillasantivergara1232@hotmail.com>
2023-11-05 19:28:26 -0300

    config: se configuro el archivo productions

---
+++ src/productions.py
@@ -0,0 +1,39 @@
+from .settings import *
+import os
+from dotenv import load_dotenv
+import dj_database_url
+
+load_dotenv()
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgres://afterflowdb_user:jP7G7t77qloOlhvOAcz2vp4x2R40tGPO@dpg-cl17peis1bgc73fo7mo0-a.oregon-postgres.render.com/afterflowdb',
+        conn_max_age=600
+    )
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'  # or any prefix you choose
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
