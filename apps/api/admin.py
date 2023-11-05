5233abd53dfa4cceb9a9359c3c48f4f6afe3c542
daniel <dvillasantivergara1232@hotmail.com>
2023-11-05 19:51:04 -0300

    Se crearon los admins para el proyecto

--- apps/api/admin.py
+++ apps/api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Post
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug']
+    list_display_links = ['id', 'name', 'slug']
+    search_fields = ['id', 'name']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'slug', 'author', 'created_on']
+    list_display_links = ['id', 'title', 'slug']
+    search_fields = ['id', 'title']
+    list_filter = ['author']
+    filter_horizontal = ['categories']
